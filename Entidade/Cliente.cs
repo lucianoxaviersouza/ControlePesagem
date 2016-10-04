7d6b393ca1a80109d78469334eccd789fbce3eac
lucianoxaviersouza <xavier.souza@gmail.com>
2016-10-03 21:46:22 -0300

    Atualização

--- Entidade/Cliente.cs
+++ Entidade/Cliente.cs
@@ -18,6 +18,8 @@ namespace Entidade
         public int UsuarioInclusao { get; set; }
         public DateTime DataInclusao { get; set; }
         public int UsuarioAlteracao { get; set; }
+
+
         public DateTime DataAlteracao { get; set; }
     }
 }
